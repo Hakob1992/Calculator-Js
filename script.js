a5d8a13873c72205ed7f5b483fc852c9ae7db1cc
Hakob192 <hakobyanstark1992@gmail.com>
2022-07-30 21:02:36 +0400

    js added

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-HTMLFormElement
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('buttons'));
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        console.log('clicked');
+        console.log(e);
+        console.log(e.target);
+        console.log(e.target.innerText);
+    });
+})
